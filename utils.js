bffda96dada8859b7ed2acb5e129cdef32a2976c
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-09 11:09:23 -0300

    feat(utils): create getpromos and price format

--- utils.js
+++ utils.js
@@ -12,9 +12,45 @@ async function getAllProductsWithPromotions(){
     return allProductsWithPromotions
 }
 
+async function getAllProductsWithTCPromotions(){
+    const productsWithPromotions = await getAllProductsWithPromotions()
+    const allProductsWithTCPromotions = []
+    productsWithPromotions.forEach( product => {
+        const promotions = product.promotions
+        promotions.forEach( promotion => {
+            if(promotion.tcenco && product.productName) { 
+                const newProduct = {
+                    productId: product.productId,
+                    productName: product.productName,
+                    normalPrice: product.prices.listPrice,
+                    offerPrice: product.prices.price,
+                    url: product.linkText,
+                    promotion: {
+                        type: promotion.type,
+                        value: promotion.value,
+                        end: promotion.end,
+                        where: promotion.description,
+                        quantity: promotion.quantity
+                    }
+                }
+                allProductsWithTCPromotions.push(newProduct) 
+            }
+        })
+    })
+    return allProductsWithTCPromotions
+}
 
+function getPriceFormatted(value){
+    const formatter = new Intl.NumberFormat('es-CL', {
+        style: 'currency',
+        currency: 'CLP',
+    })
+    const formattedPrice = formatter.format(value)
+    return formattedPrice
+}
 
 
 module.exports = {
-    getAllProductsWithPromotions
+    getAllProductsWithTCPromotions,
+    getPriceFormatted,
 }
