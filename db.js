8e901eebae5107c612af705a601555da921b817e
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-10 05:46:21 -0300

    feat(db): create db log connecting

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const uri = process.env.DB_URI
 
 function dbConnect() {
+    console.log('[DB] connecting')
     mongoose.set('strictQuery', false)
     return mongoose.connect( uri, { useNewUrlParser: true, useUnifiedTopology: true} )
 }
