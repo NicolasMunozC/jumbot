e29a316b87402c54fb4c1389e68aad09012b40d8
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-10 22:22:14 -0300

    feat(app): change testing to false

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Product = require('./models/Product')
 const { listeningBot } = require('./jumbot')
 const { sendTCPromotions, sendAllPromotions, sendActivePromotions } = require('./jobs')
 
-const testing = true
+const testing = false
 
 async function startBot(){
     await dbConnect()
@@ -15,14 +15,8 @@ async function startBot(){
     if(!testing) jumbot.start() //PRODUCTION FUNCTION
 }
 
-const jumbot = new cron.CronJob('00 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,0 * * *', async function() {
-    const currentHour = new Date().getHours()
-    console.log('[JOBS] started')
-
-    if(currentHour === 12){
-        console.log('[JOBS] free channel started')
-    }
-    console.log('[JOBS] vip channel started')
+const jumbot = new cron.CronJob('00 12 * * *', async function() {
+    await sendActivePromotions({testing: testing})
 });
 
 
