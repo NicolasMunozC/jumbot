17e428d523049b679456027efb8c07218337fad9
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-13 07:36:37 -0300

    feat(app): add version and logs

--- app.js
+++ app.js
@@ -3,26 +3,34 @@ const cron = require('cron');
 const { dbConnect } = require('./db')
 const Product = require('./models/Product')
 const { listeningBot } = require('./jumbot')
-const { sendTCPromotions, sendAllPromotions, sendActivePromotions } = require('./jobs')
+const { sendActivePromotions, sendAllPromotions } = require('./jobs');
+const { updateLog } = require('./utils');
 
 const testing = false
+const version = 1.0
 
 async function startBot(){
+    updateLog(`Bot iniciado, version:${version}.`)
     await dbConnect()
-    console.log('[DB] connected')
+    updateLog('[DB] Conectada.')
     listeningBot()
     if(testing) cronSimulator() //DEVELOP FUNCTION
     if(!testing) jumbot.start() //PRODUCTION FUNCTION
 }
 
-const jumbot = new cron.CronJob('00 12 * * *', async function() {
+const jumbot = new cron.CronJob('00 7 * * *', async function() {
+    updateLog('Se ejecutan tareas programadas...')
     await sendActivePromotions({testing: testing})
+    await sendAllPromotions({testing: testing})
+    updateLog('Tareas programadas completadas.')
 });
 
 
 async function cronSimulator(){
-    // await sendTCPromotions()
+    console.log('SE EJECUTA SIMULADOR DE TESTING');
     await sendActivePromotions({testing: testing})
+    await sendAllPromotions({testing: testing})
+    console.log('SE COMPLETO SIMULADOR DE TESTING');
 }
 
 startBot()
