d260c0faf01e4a692149064b2d453e01f135a3f8
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-13 06:57:22 -0300

    feat(servs): add savePromotion service

--- services.js
+++ services.js
@@ -1,11 +1,18 @@
 const Product = require('./models/Product')
+const Promotion = require('./models/Promotion')
 
 function fetchAllProducts(){
     return Product.find({})
 }
 
+function savePromotion(promotion){
+    const newPromotion = new Promotion(promotion)
+    return newPromotion.save()
+}
+
 
 
 module.exports = {
-    fetchAllProducts
+    fetchAllProducts,
+    savePromotion,
 }
